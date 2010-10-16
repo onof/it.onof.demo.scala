65bd35c14fe669b39a3fb20839d4f8ff4d45bad8
onofrio <onofrio@SETTEMBRE>
2010-10-16 16:34:15 +0200

    full simplify with pattern matching

--- src/it/jugancona/scalademo/PatternMatching.scala
+++ src/it/jugancona/scalademo/PatternMatching.scala
@@ -6,13 +6,26 @@ case class Number(num: Double) extends Expr                              { overr
 case class UnOp(operator: String, arg: Expr) extends Expr                { override def toString = "(" + operator + arg + ")" }
 case class BinOp(left: Expr, operator: String, right: Expr) extends Expr { override def toString = "(" + left + operator + right + ")" }
 
-class Evaluator(val context : Map[String, Double]) {	
-	def simplifyTop(expr: Expr) : Expr = expr match {
+object Simplifier {
+	// Semplifica il primo livello dell'espressione
+	private def simplifyTop(expr: Expr) : Expr = expr match {
 		case UnOp("-", UnOp("-", e))  => e // Doppia negazione
 		case BinOp(e, "+", Number(0)) => e // Aggiunta zero
 		case BinOp(e, "*", Number(1)) => e // Moltiplicazione per 1
 		case _ => expr
 	}
+	
+	// semplifica
+	def simplify(expr: Expr) : Expr = simplifyTop(
+			expr match {
+				case BinOp(a, op, b) => BinOp(simplify(a), op, simplify(b))
+				case UnOp(op, a) => UnOp(op, simplify(a))
+				case e => e
+			}
+		)
+}
+
+class Evaluator(val context : Map[String, Double]) {
 	
 	def evaluate(expr: Expr) : Double = expr match {
 		case BinOp(a, "+", b) =>  evaluate(a) + evaluate(b)
@@ -22,34 +35,38 @@ class Evaluator(val context : Map[String, Double]) {
 		case Number(a) => a
 		case Var(a) => context(a)
 	}
+	
+	def apply(expr : Expr) = evaluate(expr)
 }
 
 object EvaluatorDemo {
 	def main(args: Array[String]): Unit = {
-		// ( variable1 * (-2) ) - variable2 + 0
-		val expr = BinOp(
+		
+		val expr =
+				BinOp(
 					BinOp(
+						BinOp(
 							BinOp(
 									Var("variable1"),
 									"*", 
-									UnOp("-", Number(2))),
+									UnOp("-", BinOp(Number(2), "*", Number(1)))),
 							"-", 
 							Var("variable2")),
-					"+", 
-					Number(0)
-				)
-
+							"*", 
+							Number(1)),
+						"+",
+						Number(0))
+		
+		val simplified = Simplifier simplify expr
+		println(expr + " = " + simplified)
+		println()
+		
 		var evaluator = new Evaluator(Map(
 				"variable1" -> -5,
 				"variable2" -> 2
 				))
 		
-		val simplified = evaluator simplifyTop expr
-		println(expr + " = " + simplified)
-		
-		println()
 		println("variables = " + evaluator.context)
-		
-		println("value of " + simplified + " = " + evaluator.evaluate(simplified))
+		println("value of " + simplified + " = " + evaluator(simplified))
 	}
 }
